cc912011eb57212380854d32b045eb77abe8f47d
Jose Renteria <JoseRenteria.sf@outlook.com>
2017-09-08 22:49:29 -0700

    changed expressed route

--- app.js
+++ app.js
@@ -77,9 +77,11 @@ resultsArray.forEach(function(result) {
 var express = require('express'),
     app = express();
 
-app.get('/', function(req, res) {
+app.use('/', function(req, res) {
   res.send(resultsArray);
 })
+
+// app.use('/', express.static(__dirname + '/'));
 
 app.listen(3000, function() {
   console.log('Listening on port 3000!')
